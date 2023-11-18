63d799f776d06066d9086d5c262ed5d233476f30
Alexander Nitiola <cre8tor.alexander@gmail.com>
2023-11-18 21:26:52 +0100

    Initial Commit

---
+++ Thunderstorm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Thunderstorm
+//
+//  Created by Alexander Nitiola on 18/11/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Thunderstorm/ThunderstormApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThunderstormApp.swift
+//  Thunderstorm
+//
+//  Created by Alexander Nitiola on 18/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct ThunderstormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
