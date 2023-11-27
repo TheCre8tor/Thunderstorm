f453ac72e8c7ea840587f4e9a3615f3dfaf22b18
Alexander Nitiola <cre8tor.alexander@gmail.com>
2023-11-27 01:25:17 +0100

    integrating the CoreLocation library

--- Thunderstorm/Geocoding/GeocodingClient.swift
+++ Thunderstorm/Geocoding/GeocodingClient.swift
@@ -5,10 +5,33 @@
 //  Created by Alexander Nitiola on 26/11/2023.
 //
 
-import Foundation
+import CoreLocation
 
 final class GeocodingClient: GeocodingService {
+    // MARK: - Types
+    
+    enum GeocodingError: Error {
+        case invalidAddressString
+    }
+    
+    // MARK: - Properties
+    
+    private let geocoder = CLGeocoder()
+    
+    // MARK: - Geocoding Service
+    
     func geocodeAddressString(_ addressString: String) async throws -> [Location] {
-        Location.previews
+        guard !addressString.isEmpty else {
+            throw GeocodingError.invalidAddressString
+        }
+        
+        do {
+            let placemarks = try await geocoder.geocodeAddressString(addressString)
+            placemarks.compactMap { placemark -> Location? in
+                
+            }
+        } catch {
+            
+        }
     }
 }
